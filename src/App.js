ff8a1c22f6fc600c4b4f482df796ac60611fcd0d
-replace-all <nico95.nm@gmail.com>
2023-05-05 12:28:33 +0200

    Adding more lines

--- src/App.js
+++ src/App.js
@@ -2,9 +2,16 @@ import './App.css';
 import { useEffect, useState } from 'react';
 import { createRoot } from 'react-dom/client';
 
+export default function App({ meme }) {
+
 export default function App() {
 
   return (
+    <main>
+          <img src={meme.image} alt={meme.name} />
+<span>Top text</span>
+
+
     <div className="App">
       <div className="input">
         <label>
@@ -25,5 +32,7 @@ export default function App() {
         <button>Meme Template</button>
       </div>
     </div>
+    </main>
   );
+  }
 }
